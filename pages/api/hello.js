df20b1e77bab2e63716b3a339bc25b4bae2d62f1
kirans05 <kirans08298@gmail.com>
2023-04-05 21:19:23 +0530

    chnaged hello.js api folder

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -114,80 +114,80 @@ export default function handler(req, res) {
 
 
 
-export default async function handler(req, res) {
-  // These parameters should be used for all requests
-  const SUMSUB_APP_TOKEN = "YOUR_SUMSUB_APP_TOKEN"; // Example: sbx:uY0CgwELmgUAEyl4hNWxLngb.0WSeQeiYny4WEqmAALEAiK2qTC96fBad - Please don't forget to change when switching to production
-  const SUMSUB_SECRET_KEY = "YOUR_SUMSUB_SECRET_KEY"; // Example: Hej2ch71kG2kTd1iIUDZFNsO5C1lh5Gq - Please don't forget to change when switching to production
-  const SUMSUB_BASE_URL = "https://api.sumsub.com";
+// export default async function handler(req, res) {
+//   // These parameters should be used for all requests
+//   const SUMSUB_APP_TOKEN = "YOUR_SUMSUB_APP_TOKEN"; // Example: sbx:uY0CgwELmgUAEyl4hNWxLngb.0WSeQeiYny4WEqmAALEAiK2qTC96fBad - Please don't forget to change when switching to production
+//   const SUMSUB_SECRET_KEY = "YOUR_SUMSUB_SECRET_KEY"; // Example: Hej2ch71kG2kTd1iIUDZFNsO5C1lh5Gq - Please don't forget to change when switching to production
+//   const SUMSUB_BASE_URL = "https://api.sumsub.com";
 
-  var config = {};
-  config.baseURL = SUMSUB_BASE_URL;
+//   var config = {};
+//   config.baseURL = SUMSUB_BASE_URL;
 
-  axios.interceptors.request.use(createSignature, function (error) {
-    return Promise.reject(error);
-  });
+//   axios.interceptors.request.use(createSignature, function (error) {
+//     return Promise.reject(error);
+//   });
 
-  // This function creates signature for the request as described here: https://developers.sumsub.com/api-reference/#app-tokens
+//   // This function creates signature for the request as described here: https://developers.sumsub.com/api-reference/#app-tokens
 
-  function createSignature(config) {
-    console.log("Creating a signature for the request...");
+//   function createSignature(config) {
+//     console.log("Creating a signature for the request...");
 
-    var ts = Math.floor(Date.now() / 1000);
-    const signature = crypto.createHmac("sha256", SUMSUB_SECRET_KEY);
-    signature.update(ts + config.method.toUpperCase() + config.url);
-    signature.update(config.data);
+//     var ts = Math.floor(Date.now() / 1000);
+//     const signature = crypto.createHmac("sha256", SUMSUB_SECRET_KEY);
+//     signature.update(ts + config.method.toUpperCase() + config.url);
+//     signature.update(config.data);
 
-    config.headers["X-App-Access-Ts"] = ts;
-    config.headers["X-App-Access-Sig"] = signature.digest("hex");
+//     config.headers["X-App-Access-Ts"] = ts;
+//     config.headers["X-App-Access-Sig"] = signature.digest("hex");
 
-    return config;
-  }
-
-  function createApplicant(externalUserId, levelName) {
-    console.log("Creating an applicant...");
-
-    var method = "post";
-    var url = "/resources/applicants?levelName=" + levelName;
-    var ts = Math.floor(Date.now() / 1000);
-
-    var body = {
-      externalUserId: externalUserId,
-    };
-
-    var headers = {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      "X-App-Token": SUMSUB_APP_TOKEN,
-    };
-
-    config.method = method;
-    config.url = url;
-    config.headers = headers;
-    config.data = JSON.stringify(body);
-
-    return config;
-  }
-
-//   async function main() {
-    externalUserId =
-      "random-JSToken-" + Math.random().toString(36).substr(2, 9);
-    levelName = "basic-kyc-level";
-    console.log("External UserID: ", externalUserId);
-
-    let obj = createApplicant(externalUserId, levelName);
-
-    // response = await axios();
-    const data = await getServerSideProps({ query: obj });
-    res.status(200).json({ message: "Form submitted successfully", data });
+//     return config;
 //   }
 
-//   main();
-}
+//   function createApplicant(externalUserId, levelName) {
+//     console.log("Creating an applicant...");
 
-export async function getServerSideProps(context) {
-    return{
-        props:{
-            context
-        }
-    }
-}
+//     var method = "post";
+//     var url = "/resources/applicants?levelName=" + levelName;
+//     var ts = Math.floor(Date.now() / 1000);
+
+//     var body = {
+//       externalUserId: externalUserId,
+//     };
+
+//     var headers = {
+//       Accept: "application/json",
+//       "Content-Type": "application/json",
+//       "X-App-Token": SUMSUB_APP_TOKEN,
+//     };
+
+//     config.method = method;
+//     config.url = url;
+//     config.headers = headers;
+//     config.data = JSON.stringify(body);
+
+//     return config;
+//   }
+
+// //   async function main() {
+//     externalUserId =
+//       "random-JSToken-" + Math.random().toString(36).substr(2, 9);
+//     levelName = "basic-kyc-level";
+//     console.log("External UserID: ", externalUserId);
+
+//     let obj = createApplicant(externalUserId, levelName);
+
+//     // response = await axios();
+//     const data = await getServerSideProps({ query: obj });
+//     res.status(200).json({ message: "Form submitted successfully", data });
+// //   }
+
+// //   main();
+// }
+
+// export async function getServerSideProps(context) {
+//     return{
+//         props:{
+//             context
+//         }
+//     }
+// }
